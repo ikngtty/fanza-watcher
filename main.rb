f2c9337469cb634a7ddadbb507bad516a8d867ec
ikngtty <ikngtty@gmail.com>
2022-07-24 18:50:50 +0900

    fetch price

--- main.rb
+++ main.rb
@@ -18,7 +18,23 @@ class CLI < Thor
     puts additional_info
     title = hreview.at_css('#title').inner_text
     puts title
+
+    prices = {}
+    %w[4k hd dl st].each do |price_kind|
+      ptn = doc.at_css("##{price_kind}")
+      next unless ptn
+
+      price_area = ptn.next.at_css('.price')
+      sales_price = price_area.at_css('.tx-hangaku')&.inner_text
+      prices[price_kind] = get_price_from_text(sales_price || price_area.inner_text)
+    end
+
+    p prices
   end
+end
+
+def get_price_from_text(text)
+  text.strip.delete_suffix('円').delete(',').to_i
 end
 
 CLI.start(ARGV)
